1acb73b9aef8be6cb3608c5f3c54d397ff831da3
SriG2000 <srivatsavgopinath2000@gmail.com>
2025-06-09 17:15:31 -0500

    changed to 4.1 mini

--- app2.py
+++ app2.py
@@ -8,7 +8,7 @@ from langchain_core.output_parsers import StrOutputParser
 import streamlit as st
 
 def main():
-    llm = ChatOpenAI(model="o4-mini")
+    llm = ChatOpenAI(model="4.1-mini")
 
     recruiter_email = "Srivatsav-test@uhcstaffing.com"  # <-- Dynamically set this as needed
 
